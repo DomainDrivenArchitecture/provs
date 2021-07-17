5b8bf87ef49c3e280b65da064fdfc84c0e45e9dd
az <ansgar.zwick@meissa-gmbh.de>
2021-07-17 15:49:00 +0200

    add parameters to function call in EntryKt

--- src/main/kotlin/io/provs/entry/Entry.kt
+++ src/main/kotlin/io/provs/entry/Entry.kt
@@ -6,9 +6,9 @@ package io.provs.entry
  * Methods with a vararg parameter must be called with at least one argument.
  *
  * @param args specify class and (optionally) method and parameters, in detail:
- * @param args[0] class to be called
- * @param args[1] (optional) static method of the class; if not specified, the "main" method is used
- * @param args[2...] String parameters that are passed to the method, only applicable if method has vararg parameter
+ * @param args[0]  fully-qualified class name of the class to be called
+ * @param args[1] (optional) static method of the class with a vararg parameter of type String; if not specified, the "main" method is used
+ * @param args[2...] (optional) String parameters that are passed to the method; can be only used if method name (args[1]) is provided
  */
 fun main(vararg args: String) {
 
@@ -20,18 +20,18 @@ fun main(vararg args: String) {
         val parameterTypeStringArray = arrayOf<Class<*>>(
             Array<String>::class.java
         )
-        val method = if (args.size == 1) jClass.getMethod("main", *parameterTypeStringArray) else
-            (if (args.size == 2 && args[1] != "main") jClass.getMethod(args[1])
-            else jClass.getMethod(args[1], *parameterTypeStringArray))
+        val method = if (args.size == 1) {
+            jClass.getMethod("main", *parameterTypeStringArray)
+        } else {
+            jClass.getMethod(args[1], *parameterTypeStringArray)
+        }
 
-        if ((args.size == 1) || (args.size == 2 && args[1] == "main")) {
+        if (args.size <= 2) {
             method.invoke(null, emptyArray<String>())
-        } else if (args.size == 2) {
-            method.invoke(null)
         } else {
             method.invoke(null, args.drop(2).toTypedArray())
         }
     } else {
-        println("Usage: <packageName.className> <static methodName>")
+        println("Usage: <packageName.className> <optional static methodName> <optional parameters ... >")
     }
 }
