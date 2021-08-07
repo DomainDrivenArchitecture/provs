5f5565978565202a2f70669607e27d93b5a0faa9
az <ansgar.zwick@meissa-gmbh.de>
2021-08-07 20:23:16 +0200

    reset git to 79f60532

--- bin/main/org/domaindrivenarchitecture/provs/core/tags/Api.kt
+++
@@ -1,6 +0,0 @@
-package org.domaindrivenarchitecture.provs.core.tags
-
-@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.EXPRESSION)
-@Retention(AnnotationRetention.SOURCE)
-@Suppress("unused") // element is offered to be used externally
-annotation class Api
--- bin/main/org/domaindrivenarchitecture/provs/extensions/server_software/prometheus/base/PrometheusNginxConfig.kt
+++
@@ -1,5 +0,0 @@
-package org.domaindrivenarchitecture.provs.extensions.server_software.prometheus.base
-
-val prometheusNginxConfig = """
-                proxy_pass http://localhost:9090/prometheus;
-"""
