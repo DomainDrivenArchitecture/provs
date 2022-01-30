b3b98d8a2b3bb7f273385da874c8e703280919dd
ansgarz <ansgar.zwick@meissa-gmbh.de>
2022-01-30 20:00:09 +0100

    fix tests & introduce run/test configurations "test" (without tag containertest) and "testall" (incl. tag containertest)

--- src/test/kotlin/org/domaindrivenarchitecture/provs/server/infrastructure/K3dKtTest.kt
+++ src/test/kotlin/org/domaindrivenarchitecture/provs/server/infrastructure/K3dKtTest.kt
@@ -1,12 +1,9 @@
 package org.domaindrivenarchitecture.provs.server.infrastructure
 
-import org.domaindrivenarchitecture.provs.framework.core.docker
+import org.domaindrivenarchitecture.provs.framework.core.*
 import org.domaindrivenarchitecture.provs.framework.core.docker.containerExec
 import org.domaindrivenarchitecture.provs.framework.core.docker.provideContainer
-import org.domaindrivenarchitecture.provs.framework.core.local
 import org.domaindrivenarchitecture.provs.framework.core.processors.ContainerStartMode
-import org.domaindrivenarchitecture.provs.server.apple.appleConfig
-import org.domaindrivenarchitecture.provs.server.apple.checkAppleService
 import org.domaindrivenarchitecture.provs.server.domain.applyK8sConfig
 import org.domaindrivenarchitecture.provs.server.domain.installK3sAsContainers
 import org.domaindrivenarchitecture.provs.test.tags.ContainerTest
@@ -53,7 +50,6 @@ internal class K3dKtTest {
                 """.trimIndent())
             }
 
-
             // when
             installK3sAsContainers()
             applyK8sConfig(appleConfig())
@@ -66,3 +62,67 @@ internal class K3dKtTest {
         assertTrue(result.success)
     }
 }
+
+
+/**
+ * Checks if URL "$host/apple" is available and return text "apple"
+ */
+private fun Prov.checkAppleService(host: String = "127.0.0.1") = requireLast {
+    // repeat required as curl may return with "empty reply from server" or with "Recv failure: Connection reset by peer"
+    val res = repeatTaskUntilSuccess(12, 10) {
+        cmd("curl -m 30 $host/apple")
+    }.out?.trim()
+
+    if ("apple" == res) {
+        ProvResult(true, out = res)
+    } else {
+        ProvResult(false, err = "Url $host/apple did not return text \"apple\" but returned: $res")
+    }
+}
+
+
+fun appleConfig() =
+    """
+kind: Ingress
+apiVersion: networking.k8s.io/v1
+metadata:
+  name: apple-ingress
+  annotations:
+    kubernetes.io/ingress.class: "traefik"
+spec:
+  rules:
+  - http:
+      paths:
+      - path: /apple
+        pathType: Prefix
+        backend:
+          service: 
+            name: apple-service
+            port: 
+              number: 5678
+---
+
+kind: Pod
+apiVersion: v1
+metadata:
+  name: apple-app
+  labels:
+    app: apple
+spec:
+  containers:
+    - name: apple-app
+      image: hashicorp/http-echo
+      args:
+        - "-text=apple"
+---
+
+kind: Service
+apiVersion: v1
+metadata:
+  name: apple-service
+spec:
+  selector:
+    app: apple
+  ports:
+    - port: 5678 # Default port for image
+    """
