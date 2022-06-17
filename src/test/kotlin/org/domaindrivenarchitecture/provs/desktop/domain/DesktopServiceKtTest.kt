0659a98532a0fca8973454bd2e4b99c49c96a235
az <ansgar.zwick@meissa-gmbh.de>
2022-06-17 13:22:00 +0200

    add test provisionDesktop_fails_for_unknown_DesktopType

--- src/test/kotlin/org/domaindrivenarchitecture/provs/desktop/domain/DesktopServiceKtTest.kt
+++ src/test/kotlin/org/domaindrivenarchitecture/provs/desktop/domain/DesktopServiceKtTest.kt
@@ -2,10 +2,31 @@ package org.domaindrivenarchitecture.provs.desktop.domain
 
 import org.domaindrivenarchitecture.provs.desktop.infrastructure.getConfig
 import org.domaindrivenarchitecture.provs.test.defaultTestContainer
+import org.domaindrivenarchitecture.provs.test.tags.ContainerTest
 import org.domaindrivenarchitecture.provs.test.tags.ExtensiveContainerTest
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.assertThrows
 
 internal class DesktopServiceKtTest {
+
+    @ContainerTest
+    fun provisionDesktop_fails_for_unknown_DesktopType() {
+        // given
+        val prov = defaultTestContainer()
+
+        // when
+        val exception = assertThrows<RuntimeException> {
+            prov.provisionDesktop(
+                DesktopType("iamunkown"),
+                gitUserName = "testuser",
+                gitEmail = "testuser@test.org",
+            )
+        }
+
+        // then
+        assertEquals("No DesktopType found for value: iamunkown", exception.message)
+    }
 
     @ExtensiveContainerTest
     fun provisionDesktop() {
