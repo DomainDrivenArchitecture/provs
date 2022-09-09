7355f9f51da25fca5e0f57add42b97dbeb0e26a8
az <ansgar.zwick@meissa-gmbh.de>
2022-09-09 15:50:25 +0200

    [skip ci] remove some extensions

--- src/main/kotlin/org/domaindrivenarchitecture/provs/framework/extensions/server_software/standalone_server/prometheus/base/PrometheusNginxConfig.kt
+++
@@ -1,5 +0,0 @@
-package org.domaindrivenarchitecture.provs.framework.extensions.server_software.standalone_server.prometheus.base
-
-val prometheusNginxConfig = """
-                proxy_pass http://localhost:9090/prometheus;
-"""
