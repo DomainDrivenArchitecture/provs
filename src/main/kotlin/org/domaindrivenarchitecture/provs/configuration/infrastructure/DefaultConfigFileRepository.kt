c9b3c2a1eb1b0735e070042e0a5599de8b2a5cd0
erik <erik.seiert@meissa-gmbh.de>
2022-11-30 10:25:54 +0100

    [skip ci] mv Config file tests to config test package

--- src/main/kotlin/org/domaindrivenarchitecture/provs/configuration/infrastructure/DefaultConfigFileRepository.kt
+++ src/main/kotlin/org/domaindrivenarchitecture/provs/configuration/infrastructure/DefaultConfigFileRepository.kt
@@ -5,7 +5,6 @@ import org.domaindrivenarchitecture.provs.configuration.domain.ConfigFileName
 import org.domaindrivenarchitecture.provs.configuration.domain.ConfigFileRepository
 
 class DefaultConfigFileRepository : ConfigFileRepository {
-
     override fun assertExists(configFileName: ConfigFileName?) {
         if (configFileName != null && !checkLocalFile(configFileName.fullqualified())) {
             throw RuntimeException("Config file ${configFileName.fileName} not found. Please check if path is correct.")
