d7ad34bb832bb5d666ddda03514732ea32761028
Pat Dyn <erik.seiert@meissa-gmbh.de>
2023-02-07 08:41:04 +0000

    Check the application.yaml for errors

--- src/main/kotlin/org/domaindrivenarchitecture/provs/server/domain/k3s/ApplicationFileRepository.kt
+++ src/main/kotlin/org/domaindrivenarchitecture/provs/server/domain/k3s/ApplicationFileRepository.kt
@@ -2,4 +2,6 @@ package org.domaindrivenarchitecture.provs.server.domain.k3s
 
 interface ApplicationFileRepository {
     fun assertExists(applicationFileName: ApplicationFileName?)
+    fun assertC4kSpecError(applicationFileName: ApplicationFileName?)
+    fun assertC4kJavaException(applicationFileName: ApplicationFileName?)
 }
--- src/test/kotlin/org/domaindrivenarchitecture/provs/server/infrastructure/DefaultApplicationFileRepositoryKtTest.kt
+++ src/test/kotlin/org/domaindrivenarchitecture/provs/server/infrastructure/DefaultApplicationFileRepositoryKtTest.kt
@@ -2,6 +2,7 @@ package org.domaindrivenarchitecture.provs.server.infrastructure
 
 import org.domaindrivenarchitecture.provs.configuration.domain.ConfigFileName
 import org.domaindrivenarchitecture.provs.configuration.infrastructure.DefaultConfigFileRepository
+import org.domaindrivenarchitecture.provs.server.domain.k3s.ApplicationFileName
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -28,12 +29,78 @@ internal class DefaultApplicationFileRepositoryKtTest {
     @Test
     fun assertExistsPasses() {
         // given
-        val validFileName = "src/test/resources/existing_file"
+        val validFileName = "src/test/resources/existing-file"
 
         // when
         val validFile = ConfigFileName(File(validFileName).path)
         val repo = DefaultConfigFileRepository()
         repo.assertExists(validFile)
+
+        // then
+        // no exception is thrown
+    }
+
+    @Test
+    fun assertC4kSpecErrorThrows() {
+        // given
+        val applicationFileName = "src/test/resources/failed-spec.yaml"
+
+        // when
+        val failedFile = ApplicationFileName(File(applicationFileName).path)
+        val repo = DefaultApplicationFileRepository()
+
+        // then
+        val exception = assertThrows<RuntimeException>(
+            "Should throw because of bad spec."
+        ) { repo.assertC4kSpecError(failedFile) }
+
+        assertEquals(
+            "Application file src/test/resources/failed-spec.yaml contains spec errors. Please check your configuration file.",
+            exception.message)
+    }
+
+    @Test
+    fun assertC4kSpecErrorPasses() {
+        // given
+        val validFileName = "src/test/resources/valid.yaml"
+
+        // when
+        val validFile = ApplicationFileName(File(validFileName).path)
+        val repo = DefaultApplicationFileRepository()
+        repo.assertC4kSpecError(validFile)
+
+        // then
+        // no exception is thrown
+    }
+
+    @Test
+    fun assertC4kJavaExceptionThrows() {
+        // given
+        val applicationFileName = "src/test/resources/java-exception.yaml"
+
+        // when
+        val failedFile = ApplicationFileName(File(applicationFileName).path)
+        val repo = DefaultApplicationFileRepository()
+
+        // then
+        val exception = assertThrows<RuntimeException>(
+            "Should throw because of java exception."
+        ) { repo.assertC4kJavaException(failedFile) }
+
+        assertEquals(
+            "Application file src/test/resources/java-exception.yaml contains java exception. Please check the c4k code for errors.",
+            exception.message)
+    }
+
+    @Test
+    fun assertC4kJavaExceptionPasses() {
+        // given
+        val validFileName = "src/test/resources/valid.yaml"
+
+        // when
+        val validFile = ApplicationFileName(File(validFileName).path)
+        val repo = DefaultApplicationFileRepository()
+        repo.assertC4kJavaException(validFile)
 
         // then
         // no exception is thrown
