da73a87bcac07ca94a76d069e02aefbb80c9aa9a
ansgarz <ansgar.zwick@meissa.de>
2024-12-20 13:28:39 +0100

    fix schedule in scheduleMonthlyReboot

--- src/main/kotlin/org/domaindrivenarchitecture/provs/framework/ubuntu/scheduledjobs/domain/ScheduledJobs.kt
+++ src/main/kotlin/org/domaindrivenarchitecture/provs/framework/ubuntu/scheduledjobs/domain/ScheduledJobs.kt
@@ -9,6 +9,7 @@ import org.domaindrivenarchitecture.provs.framework.ubuntu.scheduledjobs.infrast
  * ATTENTION: Use with care!! System will be shut down, restart might not succeed in all cases.
  */
 fun Prov.scheduleMonthlyReboot() = task {
-    // reboot each first Tuesday in a month at 3:00
-    createCronJob("50_monthly_reboot", "0 2 1-7 * 2", "/sbin/shutdown", "-r now", "root")
+    // reboot each first Tuesday a month - see https://blog.healthchecks.io/2022/09/schedule-cron-job-the-funky-way/
+    val schedule = "0 2 */100,1-7 * TUE"
+    createCronJob("50_monthly_reboot", schedule, "/sbin/shutdown", "-r now", "root")
 }
