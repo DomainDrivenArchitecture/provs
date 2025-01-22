653856d629a7fd9756b3ffc1e918360a4f941720
Mirco <mirco.zachmann@meissa.de>
2025-01-22 21:05:03 +0100

    fix install shadow-cljs

--- src/main/kotlin/org/domaindrivenarchitecture/provs/desktop/infrastructure/ClojureScript.kt
+++ src/main/kotlin/org/domaindrivenarchitecture/provs/desktop/infrastructure/ClojureScript.kt
@@ -3,9 +3,26 @@ package org.domaindrivenarchitecture.provs.desktop.infrastructure
 import org.domaindrivenarchitecture.provs.framework.core.Prov
 import org.domaindrivenarchitecture.provs.framework.core.ProvResult
 import org.domaindrivenarchitecture.provs.framework.ubuntu.install.base.aptInstall
+import org.domaindrivenarchitecture.provs.framework.ubuntu.install.base.isPackageInstalled
 
 fun Prov.installShadowCljs(): ProvResult = task {
-    aptInstall("npm")
-    cmd("npm install -g npx", sudo = true)
-    cmd("npm install -g shadow-cljs", sudo = true)
+
+    if (!isPackageInstalled("npm")) {
+        aptInstall("npm")
+        cmd("sudo npm install -g npx")
+        cmd("sudo npm install -g shadow-cljs")
+    } else {
+        val npmVersion = cmd("npm --version")
+        ProvResult(true, out = "Package npm v$npmVersion already installed. Checking shadow-cljs now.")
+        if (chk("npm list -g shadow-cljs|grep empty")) {
+            cmd("sudo npm install -g shadow-cljs")
+        } else {
+            ProvResult(true, out = "Package shadow-cljs already installed.")
+        }
+        if (chk("npm list -g npx|grep empty")) {
+            cmd("sudo npm install -g npx")
+        } else {
+            ProvResult(true, out = "Package npx already installed.")
+        }
+    }
 }
