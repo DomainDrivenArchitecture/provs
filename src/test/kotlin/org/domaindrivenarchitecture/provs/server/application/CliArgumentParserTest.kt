8274405c77172193e2e9488a9e7c282f58374e79
ansgarz <ansgar.zwick@meissa.de>
2025-05-25 21:07:39 +0200

    add test_parseServerArguments_are_valid_for_k3s_remote_with_multiple_application_yaml

--- src/test/kotlin/org/domaindrivenarchitecture/provs/server/application/CliArgumentParserTest.kt
+++ src/test/kotlin/org/domaindrivenarchitecture/provs/server/application/CliArgumentParserTest.kt
@@ -48,4 +48,18 @@ internal class CliArgumentParserTest {
         assertEquals(ApplicationFileName("app.yaml").fullyQualifiedName(), result.applicationFileNames?.get(0)?.fullyQualifiedName())
         assertEquals(TargetCliCommand("user@host.com"), result.target)
     }
+
+    @Test
+    fun test_parseServerArguments_are_valid_for_k3s_remote_with_multiple_application_yaml() {
+        // given
+        val parser = CliArgumentsParser("test")
+
+        // when
+        val result: K3sCliCommand = parser.parseCommand(args = arrayOf("k3s", "user@host.com", "-a", "app.yaml,app2.yaml")) as K3sCliCommand
+
+        // then
+        assertTrue(result.isValidTarget())
+        assertEquals(listOf(ApplicationFileName("app.yaml"), ApplicationFileName("app2.yaml")).toString(), result.applicationFileNames.toString())
+        assertEquals(TargetCliCommand("user@host.com"), result.target)
+    }
 }
