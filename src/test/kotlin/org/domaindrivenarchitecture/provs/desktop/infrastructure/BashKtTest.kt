b20506d0ce9f3912d408372d90f99636ef7157a6
ansgarz <ansgar.zwick@meissa.de>
2025-06-13 20:27:21 +0200

    fix BashKtTest

--- src/test/kotlin/org/domaindrivenarchitecture/provs/desktop/infrastructure/BashKtTest.kt
+++ src/test/kotlin/org/domaindrivenarchitecture/provs/desktop/infrastructure/BashKtTest.kt
@@ -1,8 +1,8 @@
 package org.domaindrivenarchitecture.provs.desktop.infrastructure
 
 import org.domaindrivenarchitecture.provs.framework.core.getResourceAsText
-import org.domaindrivenarchitecture.provs.framework.ubuntu.filesystem.base.createFile
-import org.domaindrivenarchitecture.provs.framework.ubuntu.filesystem.base.fileContainsText
+import org.domaindrivenarchitecture.provs.framework.ubuntu.filesystem.infrastructure.createFile
+import org.domaindrivenarchitecture.provs.framework.ubuntu.filesystem.infrastructure.fileContainsText
 import org.domaindrivenarchitecture.provs.test.defaultTestContainer
 import org.domaindrivenarchitecture.provs.test.tags.ContainerTest
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -12,15 +12,17 @@ internal class BashKtTest {
 
     @ContainerTest
     fun configureBashForUser() {
+        // given
+        val prov = defaultTestContainer()
         // when
-        val res = defaultTestContainer().task {
+        val res = prov.task {
             configureBashForUser()
             createFile(".bashrc.d/testbashrcd.sh", "alias testbashrcd=\"echo -n works\"\n")
         }
         val resourcePath = "org/domaindrivenarchitecture/provs/desktop/infrastructure/"
         val expectedText = getResourceAsText(resourcePath + "bashrcd-enhancement.sh").trimIndent()
-        val containsText = defaultTestContainer().fileContainsText(".bashrc", expectedText)
-        val out = defaultTestContainer().cmd("/bin/bash -ci \". ~/.bashrc && testbashrcd\"").out
+        val containsText = prov.fileContainsText(".bashrc", expectedText)
+        val out = prov.cmd("/bin/bash -ci \". ~/.bashrc && testbashrcd\"").out
 
         // then
         assertTrue(res.success)
